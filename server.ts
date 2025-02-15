f9dda44677bed5cbc8b7a95b973b309f52dd86ff
alexandruchitoraga <alexandru.chitoraga@gmail.com>
2025-02-15 15:10:09 -0800

    Initial commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
